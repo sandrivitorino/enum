41ad29181c94e63f45017238f56862c4b2c66bcd
vonvit <sandrineviturn@gmail.com>
2026-02-21 13:36:06 -0300

    StringBuilder(excercicio de fixação)

--- composition-excfix2/src/main/java/application/Program.java
+++ composition-excfix2/src/main/java/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import entites.Comment;
+import entites.Post;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,7 +11,9 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's awesome!");
+        Post p1 = new Post(moment, title, content, likes);
 
         sc.close();
     }
